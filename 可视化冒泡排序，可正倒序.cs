0ad054ad98c5686e517e7813454824bcaef7e353
hongweihu <74164210+CNhongweiHu@users.noreply.github.com>
2023-04-16 13:23:05 +0800

    Update and rename 可视化冒泡排序，且保存了被排序前数据.cs to 可视化冒泡排序，可正倒序.cs

--- 可视化冒泡排序，可正倒序.cs
+++ 可视化冒泡排序，可正倒序.cs
@@ -8,7 +8,6 @@ namespace paramLesson
     internal class Program
     {
         //*****************************************************************
-        //*****************************************************************
         #region 冒泡排序核心逻辑
         static void BubblingSort(int[] arr)
         {
@@ -31,6 +30,38 @@ namespace paramLesson
         }
         #endregion
         //*****************************************************************
+        #region 冒泡排序 重载逻辑 可以进行倒序排序
+        static void BubblingSort(int[] arr,string arrStr)
+        {
+            bool inverted = true;
+            switch (arrStr)
+            {
+                case "inverted":
+                    break;
+                case "倒序":
+                    break;
+                default:
+                Console.WriteLine("输入错误，请输入\"inverted\"或\"倒序\"");
+                    return;
+            }
+            for (int Sort = 0; Sort < arr.Length; ++Sort)
+                {
+                    //bool isSort = false;//每次循环开始时，进行记录是否有循环 特殊情景下可以用于优化
+                    for (int i = 0; i < arr.Length; ++i)
+                    {
+                        //isSort = true;//如果本轮进行了循环，则记录有
+                        if (i < arr.Length - 1 && arr[i] < arr[i + 1])
+                        {
+                            int temp = arr[i];
+                            arr[i] = arr[i + 1];
+                            arr[i + 1] = temp;
+                            SortVisualizationSystem(arr, inverted);//调用可视化模块
+                        }
+                    }
+                    //if (!isSort) { break; }//出现了无需循环的情况，则直接结束程序
+                }
+        }
+        #endregion
         //*****************************************************************
         static void Main(string[] args)
         {
@@ -47,7 +78,7 @@ namespace paramLesson
             }
             #endregion
 
-            BubblingSort(arr);//调用冒泡排序
+            BubblingSort(arr,"倒序");//调用冒泡排序，倒序
 
             #region 打印随机前数据
             Console.WriteLine("\n本次参与排序的随机数是:");
@@ -92,6 +123,22 @@ namespace paramLesson
                 Console.Write(" {0} ", arr[visualization]);
             }
             Console.ForegroundColor= ConsoleColor.White;
+            #endregion
+        }
+        static void SortVisualizationSystem(int[] arr,bool inverted)
+        {
+            #region 打印排序情况，倒序重载
+            Console.SetCursorPosition(0, 0);
+            for (int visualization = 0; visualization < arr.Length; ++visualization)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (arr[visualization] == arr.Length - visualization)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.Write(" {0} ", arr[visualization]);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
             #endregion
         }
     }
